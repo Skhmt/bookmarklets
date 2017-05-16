caf0bcf39f56c466c175bb85d7abd61f7a27ded7
Skhmt <Skhmt@users.noreply.github.com>
2017-05-16 11:47:20 -0700

    Create make_youtube_fullscreen.js

---
+++ make_youtube_fullscreen.js
@@ -0,0 +1,6 @@
+javascript: {
+    var url = window.location.href;
+    var videoID = url.split('=')[1];
+    videoID = videoID.split('&')[0];
+    window.location.href = 'https://www.youtube.com/embed/' + videoID + '?autoplay=1';
+};
