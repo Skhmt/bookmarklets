35b91b9b4a06400f258950edf91fb7db29e08f0e
Skhmt <Skhmt@users.noreply.github.com>
2017-05-16 15:06:49 -0700

    Update make_youtube_fullscreen.js

--- make_youtube_fullscreen.js
+++ make_youtube_fullscreen.js
@@ -1,4 +1,4 @@
 javascript:
 
-let videoID = window.location.href.split('=')[1].split('&')[0];
-window.location.href = `https://www.youtube.com/embed/${videoID}?autoplay=1`;
+let videoID = location.href.split('=')[1].split('&')[0];
+location = `https://www.youtube.com/embed/${videoID}?autoplay=1`;
