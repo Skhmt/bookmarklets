7f8a00c33358b8ac3f269b9510164b1b449e3494
Skhmt <Skhmt@users.noreply.github.com>
2017-05-16 15:19:51 -0700

    Update download_vscode_extensions.js

--- download_vscode_extensions.js
+++ download_vscode_extensions.js
@@ -1,4 +1,4 @@
-javascript:
+javascript: 
 
 const vss_extension = JSON.parse(document.getElementsByClassName('vss-extension')[0].innerHTML);
 
@@ -6,19 +6,15 @@ const publisher = vss_extension.publisher.publisherName;
 const extension = vss_extension.extensionName;
 const version = vss_extension.versions[0].version;
 
-const newURL = `https://${publisher}.gallery.vsassets.io/_apis/public/gallery/publisher/${publisher}/extension/${extension}/${version}/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage`;
+let newURL = `https://${publisher}.gallery.vsassets.io/_apis/public/gallery/publisher/${publisher}`;
+newURL += `/extension/${extension}/${version}/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage`;
 
-/* http://stackoverflow.com/questions/7526849/how-do-i-change-a-filename-on-download-with-javascript */
+/* http://stackoverflow.com/questions/7526849/ */
 
-let a = document.createElement('a');
-let xhr = new XMLHttpRequest();
+const a = document.createElement('a');
 
-xhr.open('GET', newURL, true);
-xhr.responseType = 'blob';
-xhr.onload = () => {
-	const file = new Blob([xhr.response], { type : 'application/octet-stream' });
-	a.href = window.URL.createObjectURL(file);
+fetch(newURL).then(res => res.blob()).then(myBlob => {
+	a.href = window.URL.createObjectURL(myBlob);
 	a.download = `${publisher}_${extension}_${version}.vsix`;
 	a.click();
-};
-xhr.send();
+});
