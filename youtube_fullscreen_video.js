040aaf79090ca875ec02e611376e258ad1643b7f
Skhmt <Skhmt@users.noreply.github.com>
2017-05-16 19:11:23 -0700

    Update youtube_fullscreen_video.js

--- youtube_fullscreen_video.js
+++ youtube_fullscreen_video.js
@@ -1,4 +1,4 @@
-javascript:
-
-let videoID = location.href.split('v=')[1].split('&')[0];
-location = `https://www.youtube.com/embed/${videoID}?autoplay=1`;
+javascript: {
+  let videoID = location.href.split('v=')[1].split('&')[0];
+  location = `https://www.youtube.com/embed/${videoID}?autoplay=1`;
+}
